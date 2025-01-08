e15a72caac2220ba3e029e81a4b771c91bfdaf65
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-01-08 18:34:51 +0400

    Add relationship to orders

--- ECommerceAPI/Models/User.cs
+++ ECommerceAPI/Models/User.cs
@@ -8,5 +8,7 @@
         public string Email { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
+
+        public List<Order> Orders {get;set;}
     }
 }
