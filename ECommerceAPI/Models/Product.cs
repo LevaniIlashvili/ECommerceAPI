110085da779ea29f9bbf23b264fc3f7343c663c2
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-01-08 17:31:58 +0400

    Add Product Model

---
+++ ECommerceAPI/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace ECommerceAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
