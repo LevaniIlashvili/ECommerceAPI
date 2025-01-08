8289ace1f5d1c75ad8458481ab0f839c30cdc612
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-01-08 19:34:22 +0400

    change UserId type to int

--- ECommerceAPI/Models/Cart.cs
+++ ECommerceAPI/Models/Cart.cs
@@ -3,7 +3,8 @@
     public class Cart
     {
         public int Id { get; set; }
-        public string UserId { get; set; }
+        public int UserId { get; set; }
         public List<CartItem> CartItems { get; set; }
+        public User User { get; set; }
     }
 }
