99fa2cba84de07e7c4d8c2ad8c7ed29d070511d1
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-01-08 18:33:22 +0400

    Add Order model

---
+++ ECommerceAPI/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace ECommerceAPI.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime OrderDate { get; set; }
+        public OrderStatus Status { get; set; }
+        public string ShippingAddress { get; set; }
+
+        public List<OrderItem> OrderItems { get; set; }
+        public User User { get; set; }
+    }
+}
