c8afac694123573ca8d73b4f146320f891dcbc1b
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-01-08 18:54:00 +0400

    Add StoreDbContext with DbSet properties

---
+++ ECommerceAPI/Data/StoreDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ECommerceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceAPI.Data
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+        {   
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+    }
+}
