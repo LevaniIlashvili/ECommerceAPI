687fe2881f4997874fb2405f067111e17594d138
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-01-09 21:29:31 +0400

    Add category repository

---
+++ ECommerceAPI/Repositories/CategoryRepository.cs
@@ -0,0 +1,55 @@
+﻿using ECommerceAPI.Data;
+using ECommerceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceAPI.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly StoreDbContext _context;
+
+        public CategoryRepository(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category?> GetCategory(int id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task AddCategory(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateCategory(Category updatedCategory)
+        {
+            var category = await _context.Categories.FindAsync(updatedCategory.Id);
+
+            if (category != null)
+            {
+                category.Name = updatedCategory.Name;
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteCategory(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category != null)
+            {
+                _context.Remove(category);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
