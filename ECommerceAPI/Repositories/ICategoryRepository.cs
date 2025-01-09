e6fcb6b14383947a8145b386b24953ac5c968120
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-01-09 21:29:14 +0400

    Add interface for category repository

---
+++ ECommerceAPI/Repositories/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using ECommerceAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Category?> GetCategory(int id);
+        Task AddCategory(Category category);
+        Task UpdateCategory(Category updatedCategory);
+        Task DeleteCategory(int id);
+    }
+}
