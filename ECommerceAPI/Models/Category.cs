6630b2df9a571d7f4386a3cfc15612413ee381c2
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-01-11 19:58:44 +0400

    Ignore products navigation property

--- ECommerceAPI/Models/Category.cs
+++ ECommerceAPI/Models/Category.cs
@@ -1,10 +1,13 @@
-﻿namespace ECommerceAPI.Models
+﻿using System.Text.Json.Serialization;
+
+namespace ECommerceAPI.Models
 {
     public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public ICollection<Product> Products { get; set; }
+        [JsonIgnore]
+        public ICollection<Product>? Products { get; set; }
     }
 }
