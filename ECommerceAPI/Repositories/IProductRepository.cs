7a99510dc2dd33b78fe874dfbcb002dcc697704a
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-01-11 21:07:52 +0400

    Add product repository interface

---
+++ ECommerceAPI/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using ECommerceAPI.Models;
+
+namespace ECommerceAPI.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<Product?> GetProduct(int id);
+        Task<IEnumerable<Product>> GetProducts();
+        Task AddProduct(Product product);
+        Task UpdateProduct(Product updatedProduct);
+        Task DeleteProduct(int id);
+    }
+}
