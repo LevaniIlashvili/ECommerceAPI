f1c265d841ddcd73ba23c2ab5e32e2c57c94fe42
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-01-11 21:08:07 +0400

    Add product repository

---
+++ ECommerceAPI/Repositories/ProductRepository.cs
@@ -0,0 +1,59 @@
+﻿using ECommerceAPI.Data;
+using ECommerceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceAPI.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly StoreDbContext _context;
+
+        public ProductRepository(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product?> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task AddProduct(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateProduct(Product updatedProduct)
+        {
+            var product = await GetProduct(updatedProduct.Id);
+            if (product != null)
+            {
+                product.Name = updatedProduct.Name;
+                product.Description = updatedProduct.Description;
+                product.Price = updatedProduct.Price;
+                product.Stock = updatedProduct.Stock;
+                product.CategoryId = updatedProduct.CategoryId;
+            }
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteProduct(int id)
+        {
+            var product = await GetProduct(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+
+
+    }
+}
