fbc51da23d8f6056f4c4fadc2d078baef73dd4dc
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-01-13 17:49:51 +0400

    implement user repository

---
+++ ECommerceAPI/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using ECommerceAPI.Models;
+
+namespace ECommerceAPI.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> RegisterUser(User user, string password);
+        Task<User?> LoginUser(string email, string password);
+        Task<User?> GetUserById(int id);
+        Task<List<User>> GetAllUsers();
+        Task<bool> DeleteUser(int id);
+    }
+}
---
+++ ECommerceAPI/Repositories/UserRepository.cs
@@ -0,0 +1,54 @@
+﻿using ECommerceAPI.Data;
+using ECommerceAPI.Helpers;
+using ECommerceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceAPI.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly StoreDbContext _context;
+
+        public UserRepository(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> RegisterUser(User user, string password)
+        {
+            user.PasswordHash = PasswordHasher.HashPassword(password);
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<User?> LoginUser(string email, string password)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+            if (user == null || !PasswordHasher.VerifyPassword(user.PasswordHash, password))
+            {
+                return null;
+            }
+            return user;
+        }
+
+        public async Task<List<User>> GetAllUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User?> GetUserById(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<bool> DeleteUser(int id)
+        {
+            var user = await GetUserById(id);
+            if (user == null) return false;
+            _context.Users.Remove(user);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+    }
+}
