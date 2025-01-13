33b4c995f0e45143d28d3fd513726e8a4115ee37
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-01-13 17:52:54 +0400

    create helper for generating jwt tokens

---
+++ ECommerceAPI/Helpers/JwtHelper.cs
@@ -0,0 +1,34 @@
+﻿using ECommerceAPI.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ECommerceAPI.Helpers
+{
+    public class JwtHelper
+    {
+        public static string GenerateToken(User user, string secretKey, string issuer, string audience)
+        {
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.Name, user.Id.ToString()),
+            new Claim(ClaimTypes.Role, user.Role)
+        };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer,
+                audience,
+                claims,
+                expires: DateTime.Now.AddDays(7),
+                signingCredentials: credentials
+            );
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
