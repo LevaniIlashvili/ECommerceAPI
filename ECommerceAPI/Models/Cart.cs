97d724ca68fc3e44184b71adfdfe1109146c90dc
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-01-19 17:34:34 +0400

    initialize CartItems array

--- ECommerceAPI/Models/Cart.cs
+++ ECommerceAPI/Models/Cart.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public int UserId { get; set; }
-        public List<CartItem> CartItems { get; set; }
+        public List<CartItem> CartItems { get; set; } = new List<CartItem>();
         public User User { get; set; }
     }
 }
