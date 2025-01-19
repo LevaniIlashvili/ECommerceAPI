8980be46c8e21c1fc262d4c57a69f950c71be6b7
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-01-19 17:38:48 +0400

    add method for getting authenticated user id

---
+++ ECommerceAPI/Helpers/HttpContextExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+
+namespace ECommerceAPI.Helpers
+{
+    public static class HttpContextExtensions
+    {
+        public static int GetAuthenticatedUserId(this HttpContext httpContext)
+        {
+            var userIdClaim = httpContext.User?.FindFirst(ClaimTypes.Name);
+            if (userIdClaim == null)
+                throw new UnauthorizedAccessException("User is not authenticated");
+
+            return int.Parse(userIdClaim.Value);
+        }
+    }
+}
