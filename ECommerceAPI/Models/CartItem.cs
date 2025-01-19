24fbcfec133f6aae1d7e91b676c569865a607107
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-01-19 17:36:16 +0400

    add annotation for validation

--- ECommerceAPI/Models/CartItem.cs
+++ ECommerceAPI/Models/CartItem.cs
@@ -1,13 +1,18 @@
-﻿namespace ECommerceAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ECommerceAPI.Models
 {
     public class CartItem
     {
         public int Id { get; set; }
         public int CartId { get; set; }
         public int ProductId { get; set; }
+        [Range(0, int.MaxValue)]
         public int Quantity { get; set; }
 
         public Product Product { get; set; }
+        [JsonIgnore]
         public Cart Cart { get; set; }
     }
 }
