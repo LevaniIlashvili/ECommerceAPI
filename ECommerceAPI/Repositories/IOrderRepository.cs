c3a4f78035507afe01e00ca5435479847238c866
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-01-20 20:46:39 +0400

    implement order repository

---
+++ ECommerceAPI/Repositories/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using ECommerceAPI.Models;
+
+namespace ECommerceAPI.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task<Order> CreateOrder(int userId, string shippingAddress);
+        Task<IEnumerable<Order>> GetOrdersByUserId(int userId);
+        Task<Order> GetOrderById(int id);
+        Task UpdateOrderStatus(int orderId, OrderStatus orderStatus);
+    }
+}
---
+++ ECommerceAPI/Repositories/OrderRepository.cs
@@ -0,0 +1,98 @@
+﻿using ECommerceAPI.Data;
+using ECommerceAPI.DTOs;
+using ECommerceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceAPI.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly StoreDbContext _context;
+
+        public OrderRepository(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Order> CreateOrder(int userId, string shippingAddress)
+        {
+            var cart = await _context.Carts
+                .Include(c => c.CartItems)
+                .ThenInclude(ci => ci.Product)
+                .FirstOrDefaultAsync(c => c.UserId == userId);
+
+            if (cart == null)
+            {
+                throw new InvalidOperationException("The cart is empty.");
+            }
+
+            var Order = new Order
+            {
+                UserId = userId,
+                TotalPrice = cart.CartItems.Sum(ci => ci.Product.Price * ci.Quantity),
+                OrderDate = DateTime.Now,
+                Status = OrderStatus.Pending,
+                ShippingAddress = shippingAddress,
+                OrderItems = cart.CartItems.Select(ci => new OrderItem
+                {
+                    ProductId = ci.ProductId,
+                    Quantity = ci.Quantity,
+                    PriceAtPurchase = ci.Product.Price
+                }).ToList()
+            };
+
+            _context.Orders.Add(Order);
+
+            _context.CartItems.RemoveRange(cart.CartItems);
+
+            cart.CartItems.ForEach(ci => ci.Product.Stock -= ci.Quantity);
+
+            await _context.SaveChangesAsync();
+
+            return Order;
+        }
+
+        public async Task<Order> GetOrderById(int id)
+        {
+            var order = await _context.Orders.AsNoTracking().FirstOrDefaultAsync(o => o.Id == id);
+
+            if (order == null)
+            {
+                throw new ArgumentException("Order doesnt exist");
+            }
+
+            return order;
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserId(int userId)
+        {
+            var orders = await _context.Orders
+                    .AsNoTracking()
+                    .Where(o => o.UserId == userId)
+                    .Include(o => o.OrderItems)
+                    .ToListAsync();
+
+            if (orders.Count == 0)
+            {
+                throw new Exception("No orders for user");
+            }
+
+            return orders;
+
+        }
+
+        public async Task UpdateOrderStatus(int orderId, OrderStatus orderStatus)
+        {
+            var order = await _context.Orders.FindAsync(orderId);
+
+            if (order == null)
+            {
+                throw new Exception("Order doesnt exist");
+            }
+
+            order.Status = orderStatus;
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
