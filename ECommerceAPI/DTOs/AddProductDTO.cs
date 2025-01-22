869c1504b4bae640a4b1729d601d2c1645f936ac
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-01-22 20:04:57 +0400

    Add dto for product creation

---
+++ ECommerceAPI/DTOs/AddProductDTO.cs
@@ -0,0 +1,23 @@
+﻿using ECommerceAPI.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECommerceAPI.DTOs
+{
+    public class AddProductDTO
+    {
+        [Required]
+        [MinLength(2)]
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(0.01, 999999.99)]
+        public decimal Price { get; set; }
+        [Range(0, int.MaxValue)]
+        public int Stock { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int CategoryId { get; set; }
+    }
+}
