9b61f3371d5b6e7b5c2294f56c8ed4b7e5f3df13
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-01-22 20:04:41 +0400

    Add dto for category creation

---
+++ ECommerceAPI/DTOs/AddCategoryDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceAPI.DTOs
+{
+    public class AddCategoryDTO
+    {
+        [MinLength(2)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
