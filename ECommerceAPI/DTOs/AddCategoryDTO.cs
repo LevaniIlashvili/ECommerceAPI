2775f60097270b1dcbba1332e60c791d5c3236ba
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-02-07 17:35:52 +0400

    change error message

--- ECommerceAPI/DTOs/AddCategoryDTO.cs
+++ ECommerceAPI/DTOs/AddCategoryDTO.cs
@@ -4,7 +4,7 @@ namespace ECommerceAPI.DTOs
 {
     public class AddCategoryDTO
     {
-        [MinLength(2)]
+        [MinLength(2, ErrorMessage = "Category name must be at least 2 characters long")]
         public string Name { get; set; } = string.Empty;
     }
 }
