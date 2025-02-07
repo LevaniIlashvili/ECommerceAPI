822d720f5c98d024a7e3ba02f3c259be1f682827
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-02-07 17:37:10 +0400

    create class for repository result pattern

---
+++ ECommerceAPI/Helpers/RepositoryResult.cs
@@ -0,0 +1,33 @@
+﻿namespace ECommerceAPI.Helpers
+{
+    public enum RepositoryErrorType
+    {
+        ValidationError,
+        Conflict,
+        NotFound,
+        DatabaseError,
+        Unauthorized
+    }
+
+    public class RepositoryResult<T>
+    {
+        public bool Success { get; private set; }
+        public T? Data { get; private set; }
+        public string? ErrorMessage { get; private set; }
+        public RepositoryErrorType? ErrorType { get; private set; }
+
+        private RepositoryResult(bool success, T? data, string? errorMessage, RepositoryErrorType? errorType)
+        {
+            Success = success;
+            Data = data;
+            ErrorMessage = errorMessage;
+            ErrorType = errorType;
+        }
+
+        public static RepositoryResult<T> SuccessResult(T data) =>
+            new RepositoryResult<T>(true, data, null, null);
+
+        public static RepositoryResult<T> Failure(string errorMessage, RepositoryErrorType errorType) =>
+            new RepositoryResult<T>(false, default, errorMessage, errorType);
+    }
+}
