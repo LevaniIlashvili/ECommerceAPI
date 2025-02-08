fc313efbc564ad12dc99c73467ec763264b9d992
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-02-08 19:08:42 +0400

    change validation

--- ECommerceAPI/DTOs/AddProductDTO.cs
+++ ECommerceAPI/DTOs/AddProductDTO.cs
@@ -7,17 +7,19 @@ namespace ECommerceAPI.DTOs
     public class AddProductDTO
     {
         [Required]
-        [MinLength(2)]
+        [StringLength(100, MinimumLength = 3)]
         public string Name { get; set; } = String.Empty;
+
+        [StringLength(500)]
         public string Description { get; set; } = String.Empty;
 
-        [Column(TypeName = "decimal(18,2)")]
         [Range(0.01, 999999.99)]
         public decimal Price { get; set; }
+
         [Range(0, int.MaxValue)]
         public int Stock { get; set; }
 
-        [Range(1, int.MaxValue)]
+        [Required]
         public int CategoryId { get; set; }
     }
 }
