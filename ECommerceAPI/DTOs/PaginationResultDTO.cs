c8360caf19b3eac26d6b3fa2b96b1122d3578c41
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-02-16 13:17:34 +0400

    create pagination DTO

---
+++ ECommerceAPI/DTOs/PaginationResultDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerceAPI.DTOs
+{
+    public class PaginationResultDTO<T>
+    {
+        public IEnumerable<T> Products { get; set; } = new List<T>();
+        public int TotalCount { get; set; }
+        public int TotalPages { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}
