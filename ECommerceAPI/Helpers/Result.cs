7973ae4296302b90b923e0297c3f61f3bea78918
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-02-20 19:40:44 +0400

    change class name

---
+++ ECommerceAPI/Helpers/Result.cs
@@ -0,0 +1,35 @@
+﻿namespace ECommerceAPI.Helpers
+{
+    public enum ErrorType
+    {
+        ValidationError,
+        Conflict,
+        NotFound,
+        DatabaseError,
+        Unauthorized,
+        Forbid,
+        BadRequest
+    }
+
+    public class Result<T>
+    {
+        public bool Success { get; private set; }
+        public T? Data { get; private set; }
+        public string? ErrorMessage { get; private set; }
+        public ErrorType? ErrorType { get; private set; }
+
+        private Result(bool success, T? data, string? errorMessage, ErrorType? errorType)
+        {
+            Success = success;
+            Data = data;
+            ErrorMessage = errorMessage;
+            ErrorType = errorType;
+        }
+
+        public static Result<T> SuccessResult(T data) =>
+            new Result<T>(true, data, null, null);
+
+        public static Result<T> Failure(string errorMessage, ErrorType errorType) =>
+            new Result<T>(false, default, errorMessage, errorType);
+    }
+}
