fc8619d396bbe964798318f3b557cffb2afa0178
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-02-21 19:46:51 +0400

    move validation and mapping to dtos to service, implement ProductExists method

--- ECommerceAPI/Repositories/ProductRepository.cs
+++ ECommerceAPI/Repositories/ProductRepository.cs
@@ -41,22 +41,8 @@ namespace ECommerceAPI.Repositories
         }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1862:Use the 'StringComparison' method overloads to perform case-insensitive string comparisons")]
-        public async Task<RepositoryResult<Product>> AddProduct(AddProductDTO productDTO)
+        public async Task<Product> AddProduct(AddProductDTO productDTO)
         {
-            var category = await _context.Categories.FindAsync(productDTO.CategoryId);
-
-            if (category == null)
-            {
-                return RepositoryResult<Product>.Failure("The specified category doesn't exit", RepositoryErrorType.BadRequest);
-            }
-
-            var existingProduct = await _context.Products.FirstOrDefaultAsync(p => p.Name.ToLower() == productDTO.Name.ToLower());
-
-            if (existingProduct != null)
-            {
-                return RepositoryResult<Product>.Failure("Product with same name already exists", RepositoryErrorType.Conflict);
-            }
-
             var product = new Product
             {
                 Name = productDTO.Name,
@@ -64,61 +50,38 @@ namespace ECommerceAPI.Repositories
                 Price = productDTO.Price,
                 Stock = productDTO.Stock,
                 CategoryId = productDTO.CategoryId,
-                Category = category
             };
+
             await _context.Products.AddAsync(product);
             await _context.SaveChangesAsync();
-            return RepositoryResult<Product>.SuccessResult(product);
+
+            return product;
         }
 
-        public async Task<RepositoryResult<bool>> UpdateProduct(int id, AddProductDTO updatedProductDTO)
+        public async Task<bool> ProductExists(string name)
+        {
+            return await _context.Products.AnyAsync(p => p.Name == name);
+        }
+
+        public async Task UpdateProduct(int id, AddProductDTO updateProductDTO)
         {
             var product = await _context.Products.FindAsync(id);
 
-            if (product == null)
-            {
-                return RepositoryResult<bool>.Failure("Product with specified id not found", RepositoryErrorType.NotFound);
-            }
-
-            var category = await _context.Categories.FindAsync(updatedProductDTO.CategoryId);
-
-            if (category == null)
-            {
-                return RepositoryResult<bool>.Failure("Category doesn't exist", RepositoryErrorType.BadRequest);
-            }
-
-            product.Name = updatedProductDTO.Name;
-            product.Description = updatedProductDTO.Description;
-            product.Price = updatedProductDTO.Price;
-            product.Stock = updatedProductDTO.Stock;
-            product.CategoryId = updatedProductDTO.CategoryId;
+            product!.Name = updateProductDTO.Name;
+            product.Description = updateProductDTO.Description;
+            product.Price = updateProductDTO.Price;
+            product.Stock = updateProductDTO.Stock;
+            product.CategoryId = updateProductDTO.CategoryId;
 
             await _context.SaveChangesAsync();
-            return RepositoryResult<bool>.SuccessResult(true);
         }
 
-        public async Task<RepositoryResult<bool>> DeleteProduct(int id)
+        public async Task DeleteProduct(int id)
         {
             var product = await GetProduct(id);
 
-            if (product == null)
-            {
-                return RepositoryResult<bool>.Failure("Product doesn't exist", RepositoryErrorType.NotFound);
-            }
-
-            var hasActiveOrders = await _context.OrderItems.AnyAsync(oi => oi.ProductId == id);
-
-            if (hasActiveOrders)
-            {
-                return RepositoryResult<bool>.Failure("Cannot delete product with active orders", RepositoryErrorType.Conflict);
-            }
-
-            _context.Products.Remove(product);
+            _context.Products.Remove(product!);
             await _context.SaveChangesAsync();
-            return RepositoryResult<bool>.SuccessResult(true);
         }
-
-
-
     }
 }
