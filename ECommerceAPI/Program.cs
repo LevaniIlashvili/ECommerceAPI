c09f3b00d0e127284fccfb5dd569b0f4923b20a9
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-02-21 19:42:25 +0400

    add ProductService to IoC

--- ECommerceAPI/Program.cs
+++ ECommerceAPI/Program.cs
@@ -28,6 +28,8 @@ builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<ICartRepository, CartRepository>();
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
