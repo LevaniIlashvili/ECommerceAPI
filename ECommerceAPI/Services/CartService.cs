3574cb3b0758617a5a814a0a1a519327f94662ca
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-02-23 12:45:59 +0400

    implement service for cart

---
+++ ECommerceAPI/Services/CartService.cs
@@ -0,0 +1,109 @@
+﻿using ECommerceAPI.DTOs;
+using ECommerceAPI.Helpers;
+using ECommerceAPI.Models;
+using ECommerceAPI.Repositories;
+
+namespace ECommerceAPI.Services
+{
+    public class CartService : ICartService
+    {
+        private readonly ICartRepository _cartRepository;
+        private readonly IProductRepository _productRepository;
+
+        public CartService(ICartRepository cartRepository, IProductRepository productRepository)
+        {
+            _cartRepository = cartRepository;
+            _productRepository = productRepository;
+        }
+        public async Task<CartDTO> GetCartByUserId(int userId)
+        {
+            var cart = await _cartRepository.GetCartByUserId(userId);
+
+            return new CartDTO
+            {
+                UserId = userId,
+                CartItems = cart!.CartItems.Select(ci => new CartItemDTO
+                {
+                    Id = ci.Id,
+                    ProductId = ci.ProductId,
+                    Quantity = ci.Quantity
+                }).ToList()
+            };
+        }
+
+        public async Task<Result<CartItem>> AddToCart(int userId, int productId, int quantity)
+        {
+            var product = await _productRepository.GetProduct(productId);
+            if (product == null)
+            {
+                return Result<CartItem>.Failure("Product not found", ErrorType.NotFound);
+            }
+
+            if (product.Stock < quantity)
+            {
+                return Result<CartItem>.Failure("Insufficient stock", ErrorType.BadRequest);
+            }
+
+            var cart = await _cartRepository.GetCartByUserId(userId);
+             
+            var cartItem = cart.CartItems.FirstOrDefault(ci => ci.ProductId == productId);
+
+            if (cartItem != null)
+            {
+                if (product.Stock < quantity + cartItem.Quantity)
+                {
+                    return Result<CartItem>.Failure("Insufficient stock", ErrorType.BadRequest);
+                }
+                await _cartRepository.UpdateCartItem(cartItem.Id, cartItem.Quantity + quantity);
+            }
+            else
+            {
+                cartItem = await _cartRepository.AddToCart(userId, productId, quantity);
+            }
+
+            return Result<CartItem>.SuccessResult(cartItem);
+        }
+
+        public async Task<Result<bool>> UpdateCartItem(int userId, int cartItemId, int quantity)
+        {
+            var cart = await _cartRepository.GetCartByUserId(userId);
+
+            var cartItem = cart.CartItems.FirstOrDefault(ci => ci.Id == cartItemId);
+            if (cartItem == null)
+            {
+                return Result<bool>.Failure("Cart item not found", ErrorType.NotFound);
+            }
+
+            var product = await _productRepository.GetProduct(cartItem.ProductId);
+            if (product == null)
+            {
+                return Result<bool>.Failure("Product not found", ErrorType.NotFound);
+            }
+
+            if (product.Stock < quantity)
+            {
+                return Result<bool>.Failure("Insufficient stock", ErrorType.BadRequest);
+            }
+
+            await _cartRepository.UpdateCartItem(cartItemId, quantity);
+
+            return Result<bool>.SuccessResult(true);
+        }
+
+        public async Task<Result<bool>> RemoveCartItem(int userId, int cartItemId)
+        {
+            var cart = await _cartRepository.GetCartByUserId(userId);
+
+            var cartItem = cart.CartItems.FirstOrDefault(ci => ci.Id == cartItemId);
+            if (cartItem == null)
+            {
+                return Result<bool>.Failure("Cart item not found", ErrorType.NotFound);
+            }
+
+            await _cartRepository.RemoveCartItem(cartItemId);
+
+            return Result<bool>.SuccessResult(true);
+        }
+
+    }
+}
---
+++ ECommerceAPI/Services/ICartService.cs
@@ -0,0 +1,14 @@
+﻿using ECommerceAPI.DTOs;
+using ECommerceAPI.Helpers;
+using ECommerceAPI.Models;
+
+namespace ECommerceAPI.Services
+{
+    public interface ICartService
+    {
+        Task<CartDTO> GetCartByUserId(int userId);
+        Task<Result<CartItem>> AddToCart(int userId, int productId, int quantity);
+        Task<Result<bool>> UpdateCartItem(int userId, int cartItemId, int quantity);
+        Task<Result<bool>> RemoveCartItem(int userId, int cartItemId);
+    }
+}
