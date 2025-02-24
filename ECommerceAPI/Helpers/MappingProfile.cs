82f52079218aed913b8c931287e386175e5f369b
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-02-24 14:40:50 +0400

    define mappings

---
+++ ECommerceAPI/Helpers/MappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using ECommerceAPI.DTOs;
+using ECommerceAPI.Models;
+
+namespace ECommerceAPI.Helpers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Product, ProductDTO>();
+            CreateMap<ProductDTO, Product>();
+
+            CreateMap<User, UserDTO>();
+            CreateMap<UserDTO, User>();
+            CreateMap<UserRegisterDTO, User>()
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+                .ForMember(dest => dest.Role, opt => opt.Ignore());
+
+            CreateMap<Cart, CartDTO>();
+            CreateMap<CartItem, CartItemDTO>();
+
+            CreateMap<Order, OrderDTO>();
+            CreateMap<OrderItem, OrderItemDTO>();
+        }
+    }
+}
