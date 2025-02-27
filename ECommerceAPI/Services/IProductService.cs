e02fd3ed91b38178d20513be12a4ca7feae3c1cb
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-02-27 16:54:09 +0400

    change return type

--- ECommerceAPI/Services/IProductService.cs
+++ ECommerceAPI/Services/IProductService.cs
@@ -1,12 +1,11 @@
 ﻿using ECommerceAPI.DTOs;
 using ECommerceAPI.Helpers;
-using ECommerceAPI.Models;
 
 namespace ECommerceAPI.Services
 {
     public interface IProductService
     {
-        Task<Result<(IEnumerable<ProductDTO>, int)>> GetAllProductsAsync(string? category = null, int pageNumber = 1, int pageSize = 10);
+        Task<Result<PaginationResultDTO<ProductDTO>>> GetAllProductsAsync(string? category = null, int pageNumber = 1, int pageSize = 10);
         Task<ProductDTO?> GetProductByIdAsync(int id);
         Task<Result<ProductDTO>> AddProductAsync(AddProductDTO productDTO);
         Task<Result<bool>> UpdateProductAsync(int id, AddProductDTO productDTO);
