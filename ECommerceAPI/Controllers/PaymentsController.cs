54dd564317d88efda9f41d133f3028ea2e1a9dcb
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-03-06 18:35:01 +0400

    add controller for payments, implement checkout route

---
+++ ECommerceAPI/Controllers/PaymentsController.cs
@@ -0,0 +1,37 @@
+﻿using ECommerceAPI.DTOs;
+using ECommerceAPI.Helpers;
+using ECommerceAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        private readonly PaymentService _paymentService;
+
+        public PaymentsController(PaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [HttpPost("checkout")]
+        public async Task < ActionResult> CreateCheckoutSession([FromBody] CheckoutDTO checkoutDTO)
+        {
+            var userId = HttpContext.GetAuthenticatedUserId();
+            var result = await _paymentService.CreateCheckoutSession(userId, checkoutDTO.ShippingAddress, "gel");
+
+            if (!result.Success)
+            {
+                return result.ErrorType switch
+                {
+                    ErrorType.BadRequest => BadRequest(new { Message = result.ErrorMessage }),
+                    _ => StatusCode(500, new { Message = "An unexpected error occurred." })
+                };
+            };
+
+            return Ok(new { Url = result.Data });
+        }
+    }
+}
